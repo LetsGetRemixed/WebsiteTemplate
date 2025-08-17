b1a8d5c591235aac589fab6b807a9d1003d8b996
LetsGetRemixed <maveconstruction@gmail.com>
2025-08-16 21:53:32 -0500

    fix 2

--- server/functions.js
+++ server/functions.js
@@ -2,6 +2,6 @@ const functions = require('firebase-functions');
 const { app } = require('./app');
 
 // Export Express app as a single HTTPS function
-exports.api = functions.https.onRequest(app);
+exports.api = functions.region('us-central1').https.onRequest(app);
 
 
