829993a261ea7a5ddf99b6cf70da77dfb21b614c
LetsGetRemixed <maveconstruction@gmail.com>
2025-08-16 22:20:48 -0500

    fix 4

--- server/functions.js
+++ server/functions.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
 const { app } = require('./app');
+const { connectDB } = require('./config/database');
 
 // Trust proxy when running behind Cloud Functions load balancer
 app.set('trust proxy', 1);
+
+// Ensure a database connection is established on cold start
+connectDB().catch((err) => {
+  // Log but do not crash the function
+  console.error('[Functions] DB connect error:', err?.message || err);
+});
 
 // Export Express app as a single HTTPS function
 exports.api = functions.region('us-central1').https.onRequest(app);
